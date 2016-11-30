92cb4db5f8319bdaaea5b02ed7e391a3c91685e2
qkombur <qkombur@gmail.com>
2016-11-29 19:09:38 -0500

    init files

---
+++ index.js
@@ -0,0 +1,13 @@
+//index.js handles start up of app.
+
+
+
+//headers
+
+var options = {
+  headers:  {
+  'Authorization': 'Bot ' + 'MjUzMjk4MTg2MDE2MzkxMTc4.Cx-ojA.OJTTEDCreQqTe6CDH6ymI6OOnRc'
+  }
+}
+
+console.log(options.headers)
