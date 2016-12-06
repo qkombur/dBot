a3474b8ef302c7e99c0c1ec73dfdb2c5f30d282a
qkombur <qkombur@gmail.com>
2016-12-05 20:26:34 -0500

    Added models path and some scaffolding

--- index.js
+++ index.js
@@ -4,20 +4,34 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
+
 const token='MjUzMjk4MTg2MDE2MzkxMTc4.Cx-ojA.OJTTEDCreQqTe6CDH6ymI6OOnRc';
 
 bot.on('ready', () => {
+  console.log(bot.user.id);
+
   console.log('I am Ready!');
 });
 
 bot.on('message', message => {
-  console.log(message.content);
+
+  console.log(message.createdAt + " " + message.content + " " + message.channel.name );
+
+  // basic functions
   if (message.content === 'what is my avatar?'){
     message.reply(message.author.avatarURL);
   };
+
   if (message.content === 'how old am I?'){
     message.reply(message.author.createdAt);
   };
+
+  // should save message if it has the following format "x is y" and is mentioned
+
+  if (message.isMentioned(bot.user.id)){
+    message.reply("I'll remember that");
+  }
+
 
 });
 
---
+++ models/quote.js
@@ -0,0 +1,6 @@
+//todo: model database for quotes and implement database
+exports.create = function(quote) {
+  var quote =  {
+    quote: quote,
+  }
+}
