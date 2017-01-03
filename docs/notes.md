e74e5a5af7fd754151581900e77f235ae7e4db1f
qkombur <qkombur@gmail.com>
2017-01-02 22:59:07 -0500

    Now able to get facts from table and send them to chat.

--- docs/notes.md
+++ docs/notes.md
@@ -15,3 +15,6 @@ base url is https://discordapp.com/api
 
 
 forbidden cabin: https://discordapp.com/api/guilds/228658709260271616
+
+
+https://discordapp.com/oauth2/authorize?client_id=253298186016391178&scope=bot&permissions=0 add bots
--- index.js
+++ index.js
@@ -7,16 +7,33 @@ const fs = require('fs');
 const mysql = require('mysql')
 
 const bot = new Discord.Client();
+const connection = mysql.createConnection({
+
+  host     : 'localhost',
+  user     : 'myusr',
+  password : '123456',
+  database : 'mydb'
+});
+
+
 
 bot.on('ready', () => {
   console.log(bot.user.id);
+  // this connects to database.
+  connection.connect(function(err){
+  if(err){
+    console.log('Database connection error');
+  }else{
+    console.log('Database connection successful');
+  }
+});
 
   console.log('I am Ready!');
 });
 
 bot.on('message', message => {
 
-  console.log(message.createdAt + " " + message.content + " " + message.channel.name );
+  console.log(message.content);
 
   // fun example functions
   if (message.content === 'what is my avatar?'){
@@ -28,18 +45,41 @@ bot.on('message', message => {
   };
 
   // recording toggle, no off switch, because user permissions
-  if (message.content === "start recording"){
-    bot.isRecording = true;
-    message.reply("Ok this chat is being recorded.")
+  if (message.content.split("<reply>").length == 2){
+    let splitMessage = message.content.split("<reply>");
+    let factoid = {
+      fact: splitMessage[0],
+      tidbit: splitMessage[1]
+    }
+    message.reply("Ok");
+
+    connection.query('INSERT INTO facts SET ?', factoid, function(err,res){
+      if(err) throw err;
+      console.log('Last record insert id:', res.insertId);
+    });
   };
 
-  if (bot.isRecording){
-    let logMessage = message.createdAt + " - " + message.author + ": " + message.content + '\n';
-    fs.appendFile('log.txt', logMessage, 'utf8', (err) => {
-      if (err) throw err;
-    });
+  let query = "SELECT * FROM facts WHERE fact LIKE " + "'%" + message.content + "%'";
+  connection.query(query, function(err, rows, fields){
+
+    if (err) throw err;
+
+    if (rows.length > 0){
+    message.reply(rows[0].fact + rows[0].tidbit);
+  } else{
+    console.log("No facts");
   }
+  })
+  // SELECT * FROM facts WHERE fact LIKE message.content
+
+/*  if (message.content === 'print database'){
+    connection.query('SELECT * FROM facts', function(err, rows, fields){
+      if (err) throw err;
+      message.reply(rows[3].fact + rows[3].tidbit);
+    });
+  }; */
 
 });
+
 
 bot.login(token);
