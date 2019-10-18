9b9bb0723f26c21b3cc96141991915791a68cf8c
Nicholas Auger <nick@qkombur.com>
2019-10-18 22:19:46 +0000

    Refactored some logic into functions, updated sendMessage to send due to depreciation, revamped random response.

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const config = require('./config');
 const token = config.token;
 
 const bot = new Discord.Client();
-const connection = mysql.createConnection({
-
+const pool = mysql.createPool({
+  connectionLimit : 10,
   host     :  config.mysql.host,
   user     :  config.mysql.user,
   password :  config.mysql.password,
@@ -19,18 +19,21 @@ const connection = mysql.createConnection({
 });
 
 
+const totalStoredFacts = getTotalFacts()
+
+
 //Waits for ready response from Bot
 bot.on('ready', () => {
-  connection.connect(function(err){
+  pool.getConnection(function(err){
   if(err){
-    console.log('Database connection error');
+    console.log(`Database connection error:`);
+    console.log(err);
   }else{
     console.log('Database connection successful');
   }
 
   });
-  getTotalFacts();
-  console.log('I am Ready!');
+  console.log('dBot is running.');
 });
 
 bot.on('message', message => {
@@ -38,45 +41,25 @@ bot.on('message', message => {
   console.log(message.content);
   if (message.author.username !== bot.user.username){
   // fun example functions
-    if (message.content === 'what is my avatar?'){
-      message.reply(message.author.avatarURL);
-    };
+    
+    answerQuestions(message);
 
-    if (message.content === 'how old am I?'){
-      message.reply(message.author.createdAt);
-    };
-
-    // Here begins the main logic of the applicaiton,
-    // we take in messages as they come in split them into an x and y then store to the DB
-    if (message.content.split("<reply>").length == 2){
-      let splitMessage = message.content.split("<reply>");
-      let factoid = {
-        fact: splitMessage[0],
-        tidbit: splitMessage[1]
-      }
-
-      connection.query('INSERT INTO facts SET ?', factoid, function(err,res){
-        if(err) throw err;
-        console.log('Last record insert id:', res.insertId);
-        // Tells the user the message was recorded
-        message.channel.sendMessage("Ok");
-      });
-    };
+    storeFacts(message)
 
     // Then when a user types in a message that matches the results from the fact column.
     // As currently written, the entire table is queried on each message recieved. Probably not scalable.
 
-    connection.query("SELECT * FROM facts WHERE fact LIKE " + connection.escape('%' + message.content + '%'),
+    pool.query("SELECT * FROM facts WHERE fact LIKE " + pool.escape('%' + message.content + '%'),
      function(err, rows, fields){
       if (err) throw err;
       // This is a check to make sure the query actually has results.
       console.log(rows)
       if (rows.length > 0){
         if (rows.length == 1){
-          message.channel.sendMessage(rows[0].tidbit);
+          message.channel.send(rows[0].tidbit);
         }
         else{
-          message.channel.sendMessage(rows[getRandomNumber(0,rows.length)].tidbit);
+          message.channel.send(rows[getRandomNumber(0,rows.length)].tidbit);
         };
       }
       else {
@@ -87,24 +70,53 @@ bot.on('message', message => {
 
 
 // here begins the random response module
-    if ( 4 > getRandomNumber(1, getTotalFacts() )){
+  
+    if ( (totalStoredFacts / 2) > getRandomNumber(1, totalStoredFacts )){
 
-      connection.query('SELECT * FROM facts WHERE id like ' + getRandomNumber(1, getTotalFacts() ), function(err, rows, fields){
+      pool.query('SELECT * FROM facts WHERE id like ' + getRandomNumber(1, getTotalFacts() ), function(err, rows, fields){
         if (err) throw err;
-        else { message.channel.sendMessage(rows[0].tidbit); }
+        else { message.channel.send(rows[0].tidbit); }
       });
     };
-
-}
+  }
 });
 
+function storeFacts(message) {
+
+  if (message.content.split("<reply>").length == 2){
+    let splitMessage = message.content.split("<reply>");
+    let factoid = {
+      fact: splitMessage[0],
+      tidbit: splitMessage[1]
+    }
+
+    pool.query('INSERT INTO facts SET ?', factoid, function(err,res){
+      if(err) throw err;
+      console.log('Last record insert id:', res.insertId);
+      // Tells the user the message was recorded
+      message.channel.send("Ok");
+    });
+  };
+}
+
+
+function answerQuestions(message){
+
+  if (message.content === 'what is my avatar?'){
+    message.reply(message.author.avatarURL);
+  };
+
+  if (message.content === 'how old am I?'){
+    message.reply(message.author.createdAt);
+  };
+}
 
 function getRandomNumber(min, max){
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getTotalFacts(){
-  connection.query('SELECT COUNT(*) AS total FROM facts', function(err, rows, fields){
+  pool.query('SELECT COUNT(*) AS total FROM facts', function(err, rows, fields){
     if (err) throw err;
     else {
       console.log(rows);
